03264b6b9baf7164406a447e5fe75b3ea750d0da
Tullio Facchinetti <tullio.facchinetti@unipv.it>
2016-05-16 19:00:53 +0200

    Added testing (both unit and informal) scripts.

--- tests/TestFIND.py
+++ tests/TestFIND.py
@@ -1,9 +1,15 @@
 import logging
+import pprint
 from core.Model import ModelFIND
 
 logging.FileHandler('debug.log')
 logging.basicConfig(level=logging.DEBUG)
 
 m = ModelFIND()
-obj = m.load('fuzzy_system.toml')
-print(obj)
+obj = m.load('simple_find.toml')
+pprint.pprint(obj)
+
+m.input_values = [-10.0, -10.0]
+index = m.calcIndex()
+print(index)
+
