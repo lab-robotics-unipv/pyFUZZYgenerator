6a8c9ceaebc97bd9167b805d843ce78f22a6690f
Daniele De Martini <diemme89@gmail.com>
2016-05-26 14:27:08 +0200

    deleted old files

--- tests/RunExampleModel.py
+++
@@ -1,9 +0,0 @@
-import logging
-from core.Model import ModelFIND
-
-logging.FileHandler('debug.log')
-logging.basicConfig(level=logging.DEBUG)
-
-m = ModelFIND()
-obj = m.load('fuzzy_system.toml')
-print(obj)
