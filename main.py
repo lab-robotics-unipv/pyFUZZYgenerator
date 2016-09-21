cc153121dcc2b13a2f288ddaf355229e4e897458
Daniele De Martini <diemme89@gmail.com>
2016-09-21 17:29:49 +0200

    script main now takes arguments
    file names and if create a subfolder

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from core.fuzzyCreator import fuzzyCreator
+import argparse
+import os
+
+def tomlFile(string):
+		base, ext = os.path.splitext(string)
+		if ext != '.toml':
+			raise argparse.ArgumentTypeError("{} should have .toml extension".format(string))
+		return string
 
 if __name__ == '__main__':
-	fuzzyCreator('./tests/quadFis.toml', './templates/', 'tests/output').render(False)
+	parser = argparse.ArgumentParser(description='Translate TOML files into ANSI C Fuzzy library', prog='fuzzyTranslator')
+	parser.add_argument(metavar='TOML', type=tomlFile, nargs='*', help='a TOML file to translate', dest='toml')
+	parser.add_argument('-n', '--nosubforlder', action='store_true', help='disable the subfolder division')
+
+	args = parser.parse_args()
+	
+	print(args.toml)
+
+	for toml in args.toml:
+		with open(toml, 'r') as t:
+			if args.nosubforlder:
+				fuzzyCreator(t.read(), './templates/', './output').render(False)
+			else:
+				fuzzyCreator(t.read(), './templates/', './output').render(False)
 
