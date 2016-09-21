9a075aa59f77e6f61123c0353cebbec4aa7f80df
Daniele De Martini <diemme89@gmail.com>
2016-09-21 22:35:06 +0200

    added output directory as argument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from core.fuzzyCreator import fuzzyCreator
 import argparse
 import os
+from pathlib import Path
 
 def tomlFile(string):
 		base, ext = os.path.splitext(string)
@@ -8,16 +9,24 @@ def tomlFile(string):
 			raise argparse.ArgumentTypeError("{} should have .toml extension".format(string))
 		return string
 
+def outputDir(string):
+	p = Path(string)
+	if p.exists() and not p.is_dir():
+		raise argparse.ArgumentError("{} exists and it is not a directory".format(string))
+	return p
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Translate TOML files into ANSI C Fuzzy library', prog='fuzzyTranslator')
 	parser.add_argument(metavar='TOML', type=tomlFile, nargs='*', help='a TOML file to translate', dest='toml')
-	parser.add_argument('-n', '--nosubforlder', action='store_true', help='disable the subfolder division')
+	parser.add_argument('-n', '--no-subforlder', action='store_true', help='disable the subfolder division')
+	parser.add_argument('-d', '--output-dir', type=outputDir, default='./output', help='output directory')
 
 	args = parser.parse_args()
 	
-	print(args.toml)
-
 	for toml in args.toml:
 		with open(toml, 'r') as t:
-			fuzzyCreator(t.read(), './templates/', './output').render(args.nosubforlder)
+			try:
+				fuzzyCreator(t.read(), './templates/', args.output_dir).render(args.no_subforlder)
+			except:
+				raise
 
